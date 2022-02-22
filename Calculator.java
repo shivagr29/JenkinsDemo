9f96fce430eecebbbe92c7aa7d3bc13f7f7360e8
shivagr29 <shivani.agrawal@wissen.com>
2022-02-22 14:20:29 +0530

    Calculator first commit

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator
+{  
+
+    public int addition(int x, int y)
+    {
+       return x + y;
+    }
+    public int subtraction(int x, int y)
+    {
+       return x - y;
+    }
+
+}
