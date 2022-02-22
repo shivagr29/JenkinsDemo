d335d2495b84eed5f3b6b6b4e671e5156952f3fc
shivagr29 <99872228+shivagr29@users.noreply.github.com>
2022-02-22 16:18:27 +0530

    updated cal

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,13 @@
 public class Calculator
 {  
 
+     int x=10;
+    int y= 2;
     public int addition(int x, int y)
     {
-       return x + y;
+      int result = x + y;
+        return result;
+        System.out.println(result);
     }
     public int subtraction(int x, int y)
     {
